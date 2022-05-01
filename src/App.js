356ee354173ccd81e1b14ea7a5ff8100eb3e0ef9
alaminsahed <alaminsahed97@gmail.com>
2022-05-01 13:40:45 +0600

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
