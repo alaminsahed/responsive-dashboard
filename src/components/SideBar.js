e74fa238a0ea128b805a6de6086485487f1d945d
alaminsahed <alaminsahed97@gmail.com>
2022-05-07 12:56:13 +0600

    refactore active menu

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -11,19 +11,19 @@ const SideBar = () => {
 
     return (
         <div className='flex sm:grid'>
-            <div className={active === "DashBoard" ? 'flex  gap-1 cursor-pointer mb-3 w-16 lg:w-32 items-center justify-center text-green-700 sm:justify-start' : 'flex gap-1 cursor-pointer mb-3 w-16 lg:w-32 items-center justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
+            <div className={active === "DashBoard" ? 'flex  gap-1 cursor-pointer mb-3 w-16 lg:w-32 items-center justify-center text-green-700 sm:justify-start drop-shadow-2xl bg-[#FFFFFF] rounded-sm h-10' : 'flex gap-1 cursor-pointer mb-3 w-16 lg:w-32 items-center justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
                 <HomeIcon className='h-5 w-5' />
                 <span className='hidden lg:flex'>DashBoard</span>
             </div>
-            <div className={active === "Inventory" ? 'flex items-center gap-1 text-green-700 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start' : 'flex items-center gap-1 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
+            <div className={active === "Inventory" ? 'flex items-center gap-1 text-green-700 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start drop-shadow-2xl bg-[#FFFFFF] rounded-sm h-10' : 'flex items-center gap-1 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
                 <DocumentTextIcon className='h-5 w-5' />
                 <span className='hidden lg:flex'>Inventory</span>
             </div>
-            <div className={active === "Document" ? 'flex items-center gap-1 text-green-700 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start' : 'flex items-center gap-1 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
+            <div className={active === "Document" ? 'flex items-center gap-1 text-green-700 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start drop-shadow-2xl bg-[#FFFFFF] rounded-sm h-10' : 'flex items-center gap-1 cursor-pointer mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
                 <DocumentIcon className='h-5 w-5' />
                 <span className='hidden lg:flex'>Document</span>
             </div>
-            <div className={active === "Setting" ? 'flex items-center gap-1 text-green-700 mb-3 w-16 lg:w-28 justify-center sm:justify-start' : 'flex items-center gap-1 mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
+            <div className={active === "Setting" ? 'flex items-center gap-1 text-green-700 mb-3 w-16 lg:w-28 justify-center sm:justify-start drop-shadow-2xl bg-[#FFFFFF] rounded-sm h-10' : 'flex items-center gap-1 mb-3 w-16 lg:w-28 justify-center sm:justify-start'} onClick={(e) => checkActive(e.target.innerText)}>
                 <CogIcon className='h-5 w-5' />
                 <span className='hidden lg:flex'>Setting</span>
             </div>
